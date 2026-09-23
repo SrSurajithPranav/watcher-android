af820aa2b092e75255c9fe89f5a98f22212ff462
Surajith Pranav <91361858+SrSurajithPranav@users.noreply.github.com>
2026-09-23 23:56:55 +0530

    Add Watcher Android app

---
+++ app/src/main/java/com/watcher/app/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.watcher.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val WatcherGreen = Color(0xFF1F4F45)
+val WatcherGreenLight = Color(0xFF3E8E75)
+val WatcherAmber = Color(0xFFE8A33D)
+val WatcherRed = Color(0xFFD1495B)
+val WatcherBg = Color(0xFFF7F9F8)
+val WatcherSurface = Color(0xFFFFFFFF)
+val WatcherOnSurface = Color(0xFF16211E)
+
+val WatcherBgDark = Color(0xFF101613)
+val WatcherSurfaceDark = Color(0xFF1B2521)
+val WatcherOnSurfaceDark = Color(0xFFE7EFEC)
